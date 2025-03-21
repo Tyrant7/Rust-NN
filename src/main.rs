8c7fe4d3f031dbff27bbade9ef146471b4c194d9
Tyrant <tyler.sauve@bag-of-holding.com>
2025-03-21 12:59:17 -0700

    Working on XOR problem

--- src/main.rs
+++ src/main.rs
@@ -7,38 +7,41 @@ fn main() {
         Layer::new_from_rand(3, 1, relu, relu_derivative),
     ];
 
-    let inputs = Array2::from_shape_vec((2, 4), vec![
-        0., 0.,
-        1., 0.,
-        0., 1.,
-        1., 1.,
-    ]);
-    let labels = Array2::from_shape_vec((1, 4), vec![
+    let inputs = [
+        [0., 0.],
+        [1., 0.],
+        [0., 1.],
+        [1., 1.],
+    ];
+    let labels = [
         0., 
         1., 
         1., 
-        0., 
-    ]);
+        0.,
+    ];
 
     let lr = 0.01;
     let epochs = 10;
 
     for i in 0..epochs {
         for (x, label) in inputs.iter().zip(labels.iter()) {
-            println!("Input: \n{}", x);
-            println!("Label: \n{}", label);
+            println!("Input: \n{:?}", x);
+            println!("Label: \n{:?}", label);
 
-            let mut forward_signal = x.clone();
+            let x = Array2::from_shape_vec((1, x.len()), x.to_vec()).unwrap();
+            let label = Array2::from_elem((1, 1), *label);
+
+            let mut forward_signal = x;
             for layer in network.iter_mut() {
                 forward_signal = layer.forward(&forward_signal);
             }
 
             println!("Result from forward pass: \n{}", forward_signal);
-            let cost = (label - x).pow2().sum();
-            println!("Cost: {}", cost)
+            let mut error = label - forward_signal;
+            let cost = &error.pow2().sum();
+            println!("Cost: {}", cost);
 
             println!("\nBeginning backward pass...");
-            let mut error = label - &forward_signal;
 
             let mut wgrads: Vec<Array2<f32>> = Vec::new();
             let mut bgrads: Vec<Array2<f32>> = Vec::new();
@@ -100,12 +103,6 @@ impl Layer {
     }
 
     fn forward(&mut self, input: &Array2<f32>) -> Array2<f32> {
-        /*
-            println!();
-            println!("trans: \n{:?}", input.dot(&self.weights.t()));
-            println!("bias:  \n{:?}", &self.bias);
-            println!("out:   \n{:?}", input.dot(&self.weights.t()) + &self.bias);
-        */
         self.forward_input = Some(input.clone());
         let activation = (self.activation)(input.dot(&self.weights.t()) + &self.bias);
         self.forward_activations = Some(activation.clone());
@@ -113,32 +110,15 @@ impl Layer {
     }
 
     fn backward(&mut self, error: &Array2<f32>) -> (Array2<f32>, Array2<f32>, Array2<f32>) {
-
         let forward_act = self.forward_activations.as_ref().expect("Backward called before forward");
         let forward_input = self.forward_input.as_ref().expect("Backward called before forward");
 
-
-        println!();
-
-        println!("error:   {}", &error);
-
-
         let delta = error * (self.activation_derivative)(forward_act.clone());
 
-        println!("forward: {}", &forward_act);
-        println!("weight:  {}", &self.weights);
-
-        println!("delta:   {}", &delta);
-
         let wgrad = forward_input.t().dot(&delta);
-
-        println!("bias:    {}", &self.bias);
         let bgrad = delta.sum_axis(Axis(0)).insert_axis(Axis(1));
 
         let new_error = delta.dot(&self.weights);
-
-        println!("wgrad:   {}", &wgrad);
-        println!("bgrad:   {}", &bgrad);
 
         (new_error, wgrad, bgrad)
     }
