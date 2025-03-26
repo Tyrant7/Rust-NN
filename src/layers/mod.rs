20550725749d60cf72a146422d69b68b54107ba0
Tyrant <tyler.sauve@bag-of-holding.com>
2025-03-26 16:57:18 -0700

    Experiment nearing a close

--- src/layers/mod.rs
+++ src/layers/mod.rs
@@ -8,34 +8,6 @@ pub trait Layer {
     fn get_learnable_parameters(&mut self) -> Vec<Parameter> { Vec::new() }
 }
 
-pub struct SequentialLayer<T: Layer> {
-    layer: T,
-    forward_input: Option<Array2<f32>>,
-    samples: usize,
-}
-
-impl<T: Layer> SequentialLayer<T> {
-    fn new(layer: T) -> Self {
-        SequentialLayer { 
-            layer, 
-            forward_input: None,
-            samples: 0,
-        }
-    }
-
-    fn forward(&mut self, input: &Array2<f32>, train: bool) -> Array2<f32> {
-        self.forward_input = Some(input.clone());
-        self.layer.forward(input, train)
-    }
-
-    fn backward(&mut self, input: &Array2<f32>) -> Array2<f32> {
-        match &self.forward_input {
-            Some(forward) => self.layer.backward(input, forward),
-            None => panic!("Backward called before forward or outside of train mode"),
-        }
-    }
-}
-
 pub struct Parameter<'a> {
     pub value: &'a mut Array2<f32>,
     pub gradient: &'a mut Array2<f32>,
--- src/model.rs
+++ src/model.rs
@@ -1,30 +1,39 @@
 use ndarray::Array2;
 
-use crate::layers::{Layer, Parameter, SequentialLayer};
+use crate::layers::{Layer, Parameter};
 
 pub struct Model {
-    layers: Vec<SequentialLayer<Layer>>,
+    layers: Vec<Box<dyn Layer>>,
+    forward_inputs: Vec<Option<Array2<f32>>>,
     train: bool,
+    samples: usize,
 }
 
 impl Model {
     pub fn new(layers: Vec<Box<dyn Layer>>) -> Model {
+        let forward_inputs = Vec::from_iter(layers.iter().map(|_| None));
         Model {
             layers,
+            forward_inputs,
             train: true,
+            samples: 0,
         }
     }
 
     pub fn forward(&mut self, mut input: Array2<f32>) -> Array2<f32> {
-        for layer in self.layers.iter_mut() {
+        for (layer, layer_input) in self.layers.iter_mut().zip(self.forward_inputs.iter_mut()) {
+            *layer_input = Some(input.clone());
             input = layer.forward(&input, self.train);
         }
         input
     }
 
     pub fn backward(&mut self, mut error: Array2<f32>) {
-        for layer in self.layers.iter_mut().rev() {
-            error = layer.backward(&error);
+        for (layer, layer_input) in self.layers.iter_mut().zip(self.forward_inputs.iter_mut()).rev() {
+            error = match layer_input {
+                Some(forward) => layer.backward(&error, forward),
+                None => panic!("Backward called before forward or outside of train mode"),
+            }
         }
     }
 
