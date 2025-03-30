bb3d37c64f4024602ba1fa7dc259841e7d049986
Tyrant <tyler.sauve@bag-of-holding.com>
2025-03-30 10:35:35 -0700

    Fix

--- src/tests/conv.rs
+++
@@ -1,3 +0,0 @@
-pub mod conv_1d {
-    
-}
--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,1 +1,22 @@
-mod conv;
+#[cfg(test)]
+mod conv {
+    use crate::layers::Convolutional1D;
+
+    fn conv_1d {
+        let mut conv_1d = Convolutional1D::new_from_kernel(out_features, kernels, stride, padding)
+
+        let mut conv_1d = Convolutional1D::new_from_rand(7, 2, 2, 1, 0);
+    
+        let input = Array3::from_shape_vec((1, 2, 7), 
+            [0_f32, 1., 2., 3., 4., 5., 6.,
+            0_f32, 2., 4., 6., 8., 10., 12.].to_vec()).unwrap();
+        let output = conv_1d.forward(&input, true);
+        
+        let desired_output = []
+
+        println!("input:  {}", input);
+        println!("output: {}", output);
+    
+
+    }
+}
