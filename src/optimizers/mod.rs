e21830426ef5e229417a5bdb2ccfaf84ad20b298
Tyrant <tyler.sauve@bag-of-holding.com>
2025-05-11 17:19:36 -0700

    Fixed bad map call

--- src/optimizers/mod.rs
+++ src/optimizers/mod.rs
@@ -3,7 +3,7 @@ use crate::layers::LearnableParameter;
 pub trait Optimizer {
     fn step(&mut self, parameters: &mut [LearnableParameter], n_samples: usize);
     fn zero_gradients(&self, parameters: &mut [LearnableParameter]) {
-        parameters.iter_mut().map(|p| p.zero_grads());
+        parameters.iter_mut().for_each(|p| p.zero_grads());
     }
 }
 
