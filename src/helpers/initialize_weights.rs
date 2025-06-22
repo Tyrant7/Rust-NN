133392dc2c7ee9cc001988358c87ab8b2da11a21
Tyrant <tyler.sauve@bag-of-holding.com>
2025-06-22 16:37:46 -0700

    Improved kaiming initialization

--- src/helpers/initialize_weights.rs
+++ src/helpers/initialize_weights.rs
@@ -1,13 +1,28 @@
-use ndarray::{Array, IntoDimension};
+use ndarray::{Array, Dimension, IntoDimension};
 use rand::{rngs::StdRng, Rng, SeedableRng};
 use rand_distr::{Distribution, Normal};
 
-pub fn kaiming_normal<T: IntoDimension>(dims: T, use_dim: usize) -> Array<f32, <T as IntoDimension>::Dim> {
+pub enum SeedMode {
+    Seeded(u64),
+    Random,
+}
+
+impl SeedMode {
+    pub fn rng(&self) -> StdRng {
+        match self {
+            SeedMode::Seeded(seed) => StdRng::seed_from_u64(*seed),
+            SeedMode::Random => {
+                let seed = rand::random();
+                StdRng::from_seed(seed)
+            }
+        }
+    }
+}
+
+pub fn kaiming_normal<D: Dimension>(dims: D, use_dim: usize, seed_mode: SeedMode) -> Array<f32, D> {
     let dims = dims.into_dimension();
     let std_dev = (2. / dims[use_dim] as f64).sqrt();
-    let normal = Normal::new(0., std_dev).expect("Unable to create distribution");
-    
-    // TODO: Revert
-    let mut rng = StdRng::seed_from_u64(0);
-    Array::from_shape_fn(dims, |_| normal.sample(&mut rng) as f32)
+    let dist = Normal::new(0., std_dev).expect("Unable to create distribution");
+    let mut rng = seed_mode.rng();
+    Array::from_shape_fn(dims, |_| dist.sample(&mut rng) as f32)
 }
