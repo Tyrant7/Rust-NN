2276f2214790c004b61f63c23d14e780944d92eb
Tyrant <tyler.sauve@bag-of-holding.com>
2025-06-26 11:20:42 -0700

    Data augmentation core working. Will implement more varieties soon

--- src/data_management/data_augmentation.rs
+++ src/data_management/data_augmentation.rs
@@ -1,4 +1,5 @@
-use ndarray::{Array, Array1, Axis, Data, Dimension, RawDataClone};
+use ndarray::{Array, Array1, ArrayView, Axis, Data, Dimension, RawDataClone, RemoveAxis};
+use rand::Rng;
 
 pub enum AugmentationAction {
     Flip(f32, Axis),
@@ -11,17 +12,24 @@ pub struct DataAugmentation {
 }
 
 impl AugmentationAction {
-    pub fn apply<A, D>(&self, data: Array<A, D>) -> Array<A, D> 
+    pub fn apply<'a, A, D>(&self, mut data: ArrayView<'a, A, D>) -> ArrayView<'a, A, D> 
     where 
         A: Clone,
-        D: Dimension,
+        D: 'a + Dimension + RemoveAxis,
     {
-        match self {
+        let mut rng = rand::rng();
+        match *self {
             Self::Flip(temperature, axis) => {
+                if rng.random::<f32>() <= temperature {
+                    data.invert_axis(axis)
+                }
                 data
             },
             Self::Noise(temperature) => {
                 data
+                // Since iterating over all of the indices is slow, we'll instead 
+                // determine what fraction of the image should have added noise,
+                // then iterate over that many random indices and add noise that way
             },
             Self::Offset(temperature, axis) => {
                 data
@@ -35,15 +43,32 @@ impl DataAugmentation {
         DataAugmentation { actions }
     }
 
-    pub fn apply<A, D>(&self, data: Array<A, D>) -> Array<A, D> 
+    pub fn apply<'a, A, D>(&self, mut data: ArrayView<'a, A, D>) -> ArrayView<'a, A, D> 
     where 
         A: Clone,
-        D: Dimension,
+        D: 'a + Dimension + RemoveAxis,
     {
-        let mut data = data.clone();
         for action in self.actions.iter() {
             data = action.apply(data);
         }
         data
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn flip() {
+        let augmentation = DataAugmentation::new(vec![
+            AugmentationAction::Flip(1., Axis(0)),
+        ]);
+
+        let data = Array1::from_vec(vec![0., 0., 1.,]);
+        let augmented = augmentation.apply(data.view());
+
+        let target = Array1::from_vec(vec![1., 0., 0.,]);
+        assert_eq!(augmented, target);
     }
 }
