8d00675a5b5dc224685fc7e7526a05c13d6340c1
Tyrant <tyler.sauve@bag-of-holding.com>
2025-06-30 10:37:06 -0700

    Docstrings and tests for flatten.rs

--- src/layers/flatten.rs
+++ src/layers/flatten.rs
@@ -4,12 +4,30 @@ use serde::{Deserialize, Serialize};
 
 use super::{RawLayer, LearnableParameter, ParameterGroup};
 
+/// A flattening layer used to merge two adjacent axes of a tensor. 
+/// 
+/// This is typically used when transitioning from convolutional layers (which produce multi-dimensional outputs)
+/// to fully connected (linear) layers that expect 2D inputs of shape `(batch, features)`.
+/// 
+/// For example, flattening axis 2 of a tensor with shape `(batch, channels, height, width)` would produce
+/// a shape of `(batch, channels, height * width)`.
+/// 
+/// # Panics
+/// Panics if `axis` refers to the last axis, as there is no adjacent axis to merge with. 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Flatten {
     axis: usize,
 }
 
 impl Flatten {
+    /// Creates a new [`Flatten`] layer that merges the specified axis with the one following it.
+    /// 
+    /// # Arguments
+    /// - `axis`: The axis to flatten with its neighbour. For example, `axis = 1` on shape `(batch, 4, 5)`
+    ///   produces shape `(batch, 20)`.
+    /// 
+    /// # Panics
+    /// Will panic during [`Flatten::forward`] if `axis` is the last axis, since there is no adjacent axis to merge with. 
     pub fn new(axis: usize) -> Self {
         Flatten { axis }
     }
@@ -33,23 +51,38 @@ impl RawLayer for Flatten
 
     fn backward(&mut self, error: &ArrayD<f32>, forward_input: &ArrayD<f32>) -> ArrayD<f32> {
         let forward_shape = forward_input.shape();
-        error.to_shape(IxDyn(forward_shape)).unwrap().to_owned()
+        error.to_shape(IxDyn(forward_shape)).expect(
+            "Shape mismatch on error signal"
+        ).to_owned()
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use ndarray::Array1;
+    use ndarray::{Array2, Array3};
 
     use super::*;
 
     #[test]
     fn forward() {
-        // TODO
+        let mut flatten = Flatten::new(1);
+
+        let input = Array3::<f32>::zeros((1, 2, 3)).into_dyn();
+        let output = flatten.forward(&input, false);
+
+        assert_eq!(output.shape(), [1, 6]);
     }
 
     #[test]
     fn backward() {
-        // TODO
+        let mut flatten = Flatten::new(1);
+
+        let input = Array3::<f32>::zeros((1, 2, 3)).into_dyn();
+        flatten.forward(&input, false);
+
+        let error = Array2::<f32>::zeros((1, 6)).into_dyn();
+        let signal = flatten.backward(&error, &input);
+
+        assert_eq!(signal.shape(), [1, 2, 3]);
     }
 }
